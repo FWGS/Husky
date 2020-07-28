cf3295749b9829cd3ab392bef180ba37f3f75ac2
Konrad Pozniak <connyduck@users.noreply.github.com>
2020-07-28 10:38:28 +0200

    upgrade SwipeRefreshLayout to 1.1.0 (#1866)

--- app/src/main/java/com/keylesspalace/tusky/fragment/NotificationsFragment.java
+++ app/src/main/java/com/keylesspalace/tusky/fragment/NotificationsFragment.java
@@ -219,7 +219,6 @@ public class NotificationsFragment extends SFragment implements
 
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setColorSchemeResources(R.color.tusky_blue);
-        swipeRefreshLayout.setProgressBackgroundColorSchemeColor(ThemeUtils.getColor(context, android.R.attr.colorBackground));
 
         loadNotificationsFilter();
 
