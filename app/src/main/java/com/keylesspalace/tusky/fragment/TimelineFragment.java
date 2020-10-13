029513205b127f59b7b5e9a0fc51f6f4b2b2c310
Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com>
2020-10-13 18:30:39 +0200

    Fix filtering of boosts (#1954)

--- app/src/main/java/com/keylesspalace/tusky/fragment/TimelineFragment.java
+++ app/src/main/java/com/keylesspalace/tusky/fragment/TimelineFragment.java
@@ -1249,7 +1249,7 @@ public class TimelineFragment extends SFragment implements
             if (status != null
                     && ((status.getInReplyToId() != null && filterRemoveReplies)
                     || (status.getReblog() != null && filterRemoveReblogs)
-                    || shouldFilterStatus(status))) {
+                    || shouldFilterStatus(status.getActionableStatus()))) {
                 it.remove();
             }
         }
