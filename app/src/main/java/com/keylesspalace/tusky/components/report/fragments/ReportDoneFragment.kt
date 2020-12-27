08c8aee5535a3853802d301f0af368ac194e31d4
Konrad Pozniak <connyduck@users.noreply.github.com>
2020-12-27 21:25:35 +0100

    cleanup code in ConversationsFragment, SearchFragment and Report*Fragments (#2027)

--- app/src/main/java/com/keylesspalace/tusky/components/report/fragments/ReportDoneFragment.kt
+++ app/src/main/java/com/keylesspalace/tusky/components/report/fragments/ReportDoneFragment.kt
@@ -15,14 +15,11 @@
 
 package com.keylesspalace.tusky.components.report.fragments
 
-
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.fragment.app.activityViewModels
 import com.keylesspalace.tusky.R
 import com.keylesspalace.tusky.components.report.ReportViewModel
 import com.keylesspalace.tusky.components.report.Screen
@@ -34,19 +31,12 @@ import com.keylesspalace.tusky.util.show
 import kotlinx.android.synthetic.main.fragment_report_done.*
 import javax.inject.Inject
 
-
-class ReportDoneFragment : Fragment(), Injectable {
+class ReportDoneFragment : Fragment(R.layout.fragment_report_done), Injectable {
 
     @Inject
     lateinit var viewModelFactory: ViewModelFactory
 
-    private val viewModel: ReportViewModel by viewModels({ requireActivity() }) { viewModelFactory }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report_done, container, false)
-    }
+    private val viewModel: ReportViewModel by activityViewModels { viewModelFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         textReported.text = getString(R.string.report_sent_success, viewModel.accountUserName)
